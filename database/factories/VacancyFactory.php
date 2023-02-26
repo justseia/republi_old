d77b284e1897cab753fce2e170f88879ff0d1469
Сейітмұрат Қалмұрат <seiitmratkalmrat@MacBook-Air-Seiitmurat.local>
2023-02-27 00:24:24 +0600

    push

--- database/factories/VacancyFactory.php
+++ database/factories/VacancyFactory.php
@@ -23,10 +23,10 @@ class VacancyFactory extends Factory
             'location_id' => fake()->numberBetween(1, 2),
             'company_id' => fake()->numberBetween(1, 10),
             'criteria_id' => fake()->numberBetween(1, 10),
-            'responsibility' => '{"id":1}',
-            'requirement' => '{"id":1}',
-            'condition' => '{"id":1}',
-            'skill' => '{"id":1}',
+            'responsibility' => '{"title":"Обязанности","body":"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys  standard dummy text ever since the 1500s, when  an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only fi ve centuries, but also the leap into electronic t ypesetting, remaining essentially unchanged.  It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more re cently with desktop publishing software lik e Aldus PageMaker including versions  of Lorem Ipsum."}',
+            'requirement' => '{"title":"Требования","body":"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys  standard dummy text ever since the 1500s, when  an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only fi ve centuries, but also the leap into electronic t ypesetting, remaining essentially unchanged.  It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more re cently with desktop publishing software lik e Aldus PageMaker including versions  of Lorem Ipsum."}',
+            'condition' => '{"title":"Условия","body":"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys  standard dummy text ever since the 1500s, when  an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only fi ve centuries, but also the leap into electronic t ypesetting, remaining essentially unchanged.  It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more re cently with desktop publishing software lik e Aldus PageMaker including versions  of Lorem Ipsum."}',
+            'skill' => '{"title":"Ключевые навыки","body": ["Figma","Figma","Figma","Figma","Figma","Figma","Figma"]}',
         ];
     }
 }
